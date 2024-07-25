0d6c9728aa1f049f58bf8cfadd7b9b36f7d0845b
Alex <alex.safonovaa@yandex.ru>
2024-07-25 11:28:42 +0300

    Add files via upload

--- module_6_3.py
+++ module_6_3.py
@@ -1,34 +1,32 @@
 class Horse:
-
-    x_distance = 0
-    sound = 'Frrr'
+    def __init__(self):
+        self.x_distance = 0
+        self.sound = 'Frrr'
+        super().__init__()
 
     def run(self, dx):
+        self.dx = dx
         self.x_distance += dx
-
-
 class Eagle:
-
-    y_distance = 0
-    sound = 'I train, eat, sleep, and repeat'
-
+    def __init__(self):
+        self.y_distance = 0
+        self.sound = 'I train, eat, sleep, and repeat'
     def fly(self, dy):
         self.y_distance += dy
-
-
-class Pegasus(Eagle, Horse):
+class Pegasus(Horse, Eagle):
+    def __init__(self):
+        super().__init__()
     def move(self, dx, dy):
-        super().run(dx)
-        super().fly(dy)
-
+        self.run(dx)
+        self.fly(dy)
     def get_pos(self):
-        return (self.x_distance, self.y_distance)
+        return(self.x_distance, self.y_distance)
 
     def voice(self):
         print(self.sound)
 
+p1 = Pegasus
 
-# Пример использования
 p1 = Pegasus()
 
 print(p1.get_pos())
